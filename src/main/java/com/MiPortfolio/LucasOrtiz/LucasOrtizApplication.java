6bacda53030bb63800ac273ec8e29d41cdef63ff
espaarg <yoshixd444@gmail.com>
2023-05-15 15:36:25 -0300

    commit & push

--- src/main/java/com/MiPortfolio/LucasOrtiz/LucasOrtizApplication.java
+++ src/main/java/com/MiPortfolio/LucasOrtiz/LucasOrtizApplication.java
@@ -2,9 +2,6 @@ package com.MiPortfolio.LucasOrtiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LucasOrtizApplication {
@@ -13,13 +10,5 @@ public class LucasOrtizApplication {
 		SpringApplication.run(LucasOrtizApplication.class, args);
 	}
 
-        @Bean
-        	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://frontendprueba-f9914.web.app").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+        
 }
